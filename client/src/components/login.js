526eda2adf7ee2ce69146cba65172d806dcd9782
John <jbwilson94@gmail.com>
2022-03-10 19:12:40 -0500

    Version 1.0.2 -Login Page

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -4,18 +4,12 @@ export default function login() {
   return (
     <div className="form-page center">
       <div className="form-container center">
-      <h1>Login</h1>
+      <h1>Sign In</h1>
         <form className="form">
-          <label for="id" className="left-align">
-            Email
-          </label>
-          <input type="text" id="id"></input>
-          <label for="password" className="left-align">
-            Password
-          </label>
-          <input type="text" id="password"></input>
-          <button className="primary-blue">Login</button>
-          <button className="secondary-grey">Sign Up</button>
+          <input id="inputEmail" type="email" placeholder="Email Address" required autoFocus></input>
+          <input id="inputPassword" type="password" placeholder="Password" required></input>
+          <button id="login-button">Login</button>
+          <button id="signup-button">Sign Up</button>
         </form>
       </div>
     </div>
