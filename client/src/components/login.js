283d6ba45155de6a80091d2a1ff4a12d59b84387
John <jbwilson94@gmail.com>
2022-03-11 21:01:31 -0500

    Version 1.0.4 -Login Page: Light/Dark icon added

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons'
 
 export default function login() {
+  function changeLightMode() {
+    let lightColor = "#e3e3e3";
+    let darkColor = "e3e3e3"; 
+    document.getElementById("form-page").style.backgroundColor = "#121212";
+    document.getElementById("light-dark-icon-button").style.backgroundColor = "#121212";
+    document.getElementById("form-h1").style.color = "#e3e3e3";
+    document.getElementById("light-dark-icon").style.color = "#e3e3e3";
+  }
+
   return (
-    <div className="form-page center">
+    <div id="form-page" className="form-page center">
+      <button id="light-dark-icon-button" onClick={changeLightMode}>
+        <FontAwesomeIcon id="light-dark-icon" icon={faCircleHalfStroke} />
+      </button>
       <div className="form-container center">
-      <h1>Sign In</h1>
+      <h1 id="form-h1">Sign In</h1>
         <form className="form">
           <input id="inputEmail" type="email" placeholder="Email Address" required autoFocus></input>
           <input id="inputPassword" type="password" placeholder="Password" required></input>
