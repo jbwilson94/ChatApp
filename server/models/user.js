f8ed003c2af23625bc6cc34bb95f5689408c08b6
jbwilson94 <80640793+jbwilson94@users.noreply.github.com>
2022-03-18 15:05:58 -0400

    Version 1.1.4 -Backend userroutes complete

--- server/models/user.js
+++ server/models/user.js
@@ -1,40 +1,41 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
-const passportLocalMongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const Session = new Schema({
-    refreshToken: {
-        type: String,
-        default: "",
-    },
-});
+  refreshToken: {
+    type: String,
+    default: "",
+  },
+})
 
 const User = new Schema({
-    firstName: {
-        type: String,
-        default: "",
-    },
-    email: {
-        type: String,
-        default: "",
-    },
-    authStrategy: {
-        type: String,
-        default: "local",
-    },
-    refreshToken: {
-        type: [Session],
-    },
-});
+  firstName: {
+    type: String,
+    default: "",
+  },
+  lastName: {
+    type: String,
+    default: "",
+  },
+  authStrategy: {
+    type: String,
+    default: "local",
+  },    
+  refreshToken: {
+    type: [Session],
+  },
+})
 
-// Remove refreshToken from the response
+//Remove refreshToken from the response
 User.set("toJSON", {
-    tranform: function(doc, ret, options) {
-        delete ret.refreshToken;
-        return ret;
-    },
-});
+  transform: function (doc, ret, options) {
+    delete ret.refreshToken
+    return ret
+  },
+})
 
-User.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User", User);
+User.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model("User", User)
