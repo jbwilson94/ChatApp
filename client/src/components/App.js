a7cd0bacb79591368c9b5ce1290146174bdd684e
jbwilson94 <80640793+jbwilson94@users.noreply.github.com>
2022-03-22 14:52:07 -0400

    Version 1.1.6

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,56 @@
-import { useState } from "react";
+import { useCallback, useState, useContext, useEffect } from "react";
+import { UserContext } from "../context/UserContext";
 import SignUp from "./SignUp";
 import Login from "./Login";
+import Welcome from "./Welcome";
 import "../styles/style.css";
 
 function App() {
   const [renderLogin, setRenderLogin] = useState(true);
+  const [userContext, setUserContext] = useContext(UserContext);
 
-  return (
+  const verifyUser = useCallback(() => {
+    fetch(process.env.REACT_APP_API_ENDPOINT + "users/refreshToken", {
+      method: "POST",
+
+      credentials: "include",
+
+      headers: { "Content-Type": "application/json" },
+    }).then(async (response) => {
+      if (response.ok) {
+        const data = await response.json();
+
+        setUserContext((oldValues) => {
+          return { ...oldValues, token: data.token };
+        });
+      } else {
+        setUserContext((oldValues) => {
+          return { ...oldValues, token: null };
+        });
+      }
+
+      // call refreshToken every 5 minutes to renew the authentication token.
+      setTimeout(verifyUser, 5 * 60 * 1000);
+    });
+  }, [setUserContext]);
+
+  useEffect(() => {
+    verifyUser();
+  }, [verifyUser]);
+
+  return userContext.token === null ? (
     <div>
+      {userContext.token +"gggggggggggggg"}
       {renderLogin ? (
-        <Login
-          changeView={setRenderLogin}
-        />
+        <Login changeView={setRenderLogin} />
       ) : (
-        <SignUp
-          changeView={setRenderLogin}
-        />
+        <SignUp changeView={setRenderLogin} />
       )}
+    </div>
+  ) : (
+    <div>
+      {userContext.token}
+      <Welcome />
     </div>
   );
 }
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,24 +1,65 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { UserContext } from "../context/UserContext";
 
 export default function Login({ changeView }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userContext, setUserContext] = useState(UserContext);
 
   function handleSignUpClick() {
     changeView(false);
   }
 
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    setError("");
+
+    const genericErrorMessage = "Something went wrong! Please try again later.";
+    fetch(process.env.REACT_APP_API_ENDPOINT + "users/login", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: email, password }),
+    })
+      .then(async (response) => {
+        setIsSubmitting(false);
+        if (!response.ok) {
+          if (response.status === 400) {
+            setError("Please fill all the fields correctly!");
+          } else if (response.status === 401) {
+            setError("Invalid email and password combination.");
+          } else {
+            console.log("hi");
+            setError(genericErrorMessage);
+          }
+        } else {
+          const data = await response.json();
+          setUserContext((oldValues) => {
+            return { ...oldValues, token: data.token };
+          });
+        }
+      })
+      .catch((error) => {
+        setIsSubmitting(false);
+        setError(genericErrorMessage);
+      });
+  };
+
   return (
     <div id="form-page" className="form-page center">
       <div className="form-container center">
         <h1 id="form-h1">Sign In</h1>
-        <form className="form">
+        {error}
+        <form onSubmit={formSubmitHandler} className="form">
           <input
             id="email"
             type="email"
             placeholder="Email Address"
             value={email}
-            onChange={e => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             required
             autoFocus
           ></input>
@@ -27,12 +68,16 @@ export default function Login({ changeView }) {
             type="password"
             placeholder="Password"
             value={password}
-            onChange={e => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
             required
           ></input>
           <button id="login-button">Login</button>
         </form>
-        <button id="signup-button" onClick={handleSignUpClick}>
+        <button
+          id="signup-button"
+          disabled={isSubmitting}
+          onClick={handleSignUpClick}
+        >
           Sign Up
         </button>
       </div>
